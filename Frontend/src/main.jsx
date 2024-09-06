557eea568d17c066bd936f5ae5c6a4a52b87b840
Kunal Kumar <kumar180kunal@gmail.com>
2024-09-06 21:11:42 +0530

    Update

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,17 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-import { GlobalState } from './store/GlobalStore.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { GlobalState } from "./store/GlobalStore.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <GlobalState>
-    <App />
-
-    </GlobalState>
+      <GlobalState>
+        <App />
+      </GlobalState>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </StrictMode>
+);
