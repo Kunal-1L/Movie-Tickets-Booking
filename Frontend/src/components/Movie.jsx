015bbfef94210fe3586570260d14d8b2f33f744f
Kunal Kumar <kumar180kunal@gmail.com>
2024-09-07 14:06:15 +0530

    Update App

--- Frontend/src/components/Movie.jsx
+++ Frontend/src/components/Movie.jsx
@@ -1,18 +1,21 @@
-
-import styles from './Movie.module.css'
-import { useNavigate } from 'react-router-dom'
-const Movie = ({movie}) => { 
-    const navigate = useNavigate();
-    const handleMovie = () => {
-        navigate(`/movie/${movie._id}`)
-    }
-    return (
-        <div onClick = {handleMovie} className={styles.movie_container}>
-            <div className={styles.movie_title}>{movie.title}</div>
-            <div className={styles.movie_poster}><img src={movie.poster_url}></img></div>
-            <div className={styles.movie_rating}>{movie.rating}</div>
+import styles from "./Movie.module.css";
+import { useNavigate } from "react-router-dom";
+const Movie = ({ movie }) => {
+  const navigate = useNavigate();
+  const handleMovie = () => {
+    navigate(`/movie/${movie._id}`);
+  };
+  return (
+    <>
+      <div onClick={handleMovie} className={styles.movie_container}>
+        <div className={styles.movie_poster}>
+          <img src={movie.poster_url}></img>
         </div>
-    )
-}
+        <div className={styles.movie_rating}>{movie.rating}</div>
+      </div>
+      <div className={styles.movie_title}>{movie.title}</div>
+    </>
+  );
+};
 
 export default Movie;
